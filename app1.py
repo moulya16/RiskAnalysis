33666596d8980a59b9a1c5cc1c2b4ea42426fffb
moulya16 <85551208+moulya16@users.noreply.github.com>
2022-06-20 09:27:34 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -22,12 +22,12 @@ y = train.Loan_Status
 
 
 from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state = 42)
+x_train, x_test, y_train, y_test = train_test_split(X,y, test_size=0.1, random_state = 41)
 
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-rf = RandomForestClassifier(max_depth=4,random_state=42)
+rf = RandomForestClassifier(max_depth=4,random_state=41)
 rf.fit(x_train, y_train)
 print("Accuracy : ",rf.score(x_test, y_test))
 
